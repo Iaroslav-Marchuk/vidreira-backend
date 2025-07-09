76c9d0b502eb1ff67ce63b09a72d52613a2819e8
iaroslav_marchuk <jarik.13a@gmail.com>
2025-07-09 21:44:27 +0100

    add work

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,19 @@ import mongoose from 'mongoose';
 
 import { getEnvVariable } from '../utils/getEnvVariable.js';
 
-const DB_URI = getEnvVariable('DB_URI');
+export const initDatabaseConection = async () => {
+  try {
+    const user = getEnvVariable('MONGODB_USER');
+    const pwd = getEnvVariable('MONGODB_PASSWORD');
+    const url = getEnvVariable('MONGODB_URL');
+    const db = getEnvVariable('MONGODB_DB');
 
-async function initDatabaseConection() {
-  await mongoose.connect(DB_URI);
-}
-
-export { initDatabaseConection };
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log('Error while setting up mongo connection', error);
+    throw error;
+  }
+};
