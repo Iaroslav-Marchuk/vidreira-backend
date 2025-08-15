bf67fef9cd35ede5bbd4802b085d22e06616a71b
iaroslav_marchuk <jarik.13a@gmail.com>
2025-08-15 23:11:25 +0100

    add work

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 
+const allowedOrigin = 'http://localhost:5173/'; //---------------змінити адресу після деплою
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: allowedOrigin, credentials: true }));
+
 app.use(cookieParser());
 
 app.use(
