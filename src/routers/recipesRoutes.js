046b114b40874a585b1bd8847123e8cc32855850
iaroslav_marchuk <jarik.13a@gmail.com>
2025-08-23 01:04:31 +0100

    add work

--- src/routers/recipesRoutes.js
+++ src/routers/recipesRoutes.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import { authenticante } from '../middlewares/authenticante.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { getRecipeByIdController } from '../controllers/recipesControllers.js';
 import { isValidIdRecipe } from '../middlewares/isValidIdRecipe.js';
 
 const router = Router();
-
-router.use(authenticante);
 
 router.get('/:recipeId', isValidIdRecipe, ctrlWrapper(getRecipeByIdController));
 
