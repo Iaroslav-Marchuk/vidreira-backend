26c5311e223d85773335dee089d8ad2cf97b022a
iaroslav_marchuk <jarik.13a@gmail.com>
2025-09-02 19:39:54 +0100

    add work

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,15 +3,11 @@ import { Router } from 'express';
 import orderRouter from './ordersRoutes.js';
 import authRouter from './authRoutes.js';
 import historyRouter from './historyRoutes.js';
-import recipeRouter from './recipesRoutes.js';
 
 const router = Router();
 
 router.use('/orders', orderRouter);
 router.use('/auth', authRouter);
 router.use('/history', historyRouter);
-
-//це видалити після командного проекту
-router.use('/recipes', recipeRouter);
 
 export default router;
