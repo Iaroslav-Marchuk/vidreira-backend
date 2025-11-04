e6874e579ecc89ad8840e74fadc39ce1fa24a729
iaroslav_marchuk <jarik.13a@gmail.com>
2025-11-04 22:18:29 +0000

    add work

--- src/services/clientServices.js
+++ src/services/clientServices.js
@@ -1,10 +1,26 @@
+// import createHttpError from 'http-errors';
+// import { ClientModel } from '../models/clientModel.js';
+
+// export const getAllClientsService = async () => {
+//   const clients = await ClientModel.find();
+//   if (!clients) {
+//     throw createHttpError(404, 'Clients not found!');
+//   }
+//   return clients;
+// };
+
 import createHttpError from 'http-errors';
 import { ClientModel } from '../models/clientModel.js';
 
 export const getAllClientsService = async () => {
-  const clients = await ClientModel.find();
-  if (!clients) {
+  const clients = await ClientModel.find()
+    .sort({ name: 1 })
+    .collation({ locale: 'pt' })
+    .exec();
+
+  if (!clients || clients.length === 0) {
     throw createHttpError(404, 'Clients not found!');
   }
+
   return clients;
 };
